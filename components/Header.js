8fa29a624da1af589420a37f7c404709a29c9c97
Joshua Farin <joshua.k.farin@gmail.com>
2022-05-16 11:55:26 -1000

    add search function

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import { MicrophoneIcon, XIcon } from '@heroicons/react/solid';
+import { MicrophoneIcon, SearchIcon, XIcon } from '@heroicons/react/solid';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useRef } from 'react';
@@ -7,6 +7,15 @@ function Header() {
   const router = useRouter();
   const searchInputRef = useRef(null);
 
+  const search = e => {
+      e.preventDefault();
+
+      const term = searchInputRef.current.value;
+
+      if (!term) return;
+
+      router.push(`/search?term=${term}`);
+  }
   return (
     <div>
       <header>
@@ -24,6 +33,8 @@ function Header() {
               onClick={() => (searchInputRef.current.value = "")}
               />
               <MicrophoneIcon className='mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300'/>
+              <SearchIcon className='h-6 text-blue-500 hidden sm:inline-flex'/>
+              <button hidden type='submit' onClick={search}>Search</button>
           </form>
       </header>
     </div>
